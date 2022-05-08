ad5d7cd621f917c414258978bf2851e14e0c3649
Alex Keefe <mxer14@gmail.com>
2022-05-07 23:01:34 -0700

    Add project files.

---
+++ AutoWinForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AutoKey
+{
+    partial class AutoWinWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoWinWindow));
+            this.IsEnabledCheckbox = new System.Windows.Forms.CheckBox();
+            this.RemoveSelectedBtn = new System.Windows.Forms.Button();
+            this.MoveBar = new System.Windows.Forms.Button();
+            this.Minimize = new System.Windows.Forms.Button();
+            this.AdditionBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RunAtStartupChkBox = new System.Windows.Forms.CheckBox();
+            this.alphaBlendTextBox1 = new ZBobb.AlphaBlendTextBox();
+            this.ApplicationListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // IsEnabledCheckbox
+            // 
+            this.IsEnabledCheckbox.AutoSize = true;
+            this.IsEnabledCheckbox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.IsEnabledCheckbox.Checked = true;
+            this.IsEnabledCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.IsEnabledCheckbox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.IsEnabledCheckbox.Location = new System.Drawing.Point(25, 42);
+            this.IsEnabledCheckbox.Name = "IsEnabledCheckbox";
+            this.IsEnabledCheckbox.Size = new System.Drawing.Size(103, 17);
+            this.IsEnabledCheckbox.TabIndex = 0;
+            this.IsEnabledCheckbox.Text = "Enable AutoWin";
+            this.IsEnabledCheckbox.UseVisualStyleBackColor = false;
+            this.IsEnabledCheckbox.CheckedChanged += new System.EventHandler(this.EnabledCheckbox_CheckedChanged);
+            // 
+            // RemoveSelectedBtn
+            // 
+            this.RemoveSelectedBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.RemoveSelectedBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RemoveSelectedBtn.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemoveSelectedBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.RemoveSelectedBtn.Location = new System.Drawing.Point(243, 215);
+            this.RemoveSelectedBtn.Name = "RemoveSelectedBtn";
+            this.RemoveSelectedBtn.Size = new System.Drawing.Size(200, 35);
+            this.RemoveSelectedBtn.TabIndex = 2;
+            this.RemoveSelectedBtn.Text = "Remove Selected";
+            this.RemoveSelectedBtn.UseVisualStyleBackColor = false;
+            this.RemoveSelectedBtn.Click += new System.EventHandler(this.RemoveSelectedBtn_Click);
+            // 
+            // MoveBar
+            // 
+            this.MoveBar.BackColor = System.Drawing.Color.Transparent;
+            this.MoveBar.BackgroundImage = global::AutoKey.Properties.Resources.backgroundtest11;
+            this.MoveBar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MoveBar.FlatAppearance.BorderSize = 0;
+            this.MoveBar.ForeColor = System.Drawing.Color.Transparent;
+            this.MoveBar.Location = new System.Drawing.Point(-10, -10);
+            this.MoveBar.Name = "MoveBar";
+            this.MoveBar.Size = new System.Drawing.Size(528, 46);
+            this.MoveBar.TabIndex = 4;
+            this.MoveBar.TabStop = false;
+            this.MoveBar.UseVisualStyleBackColor = false;
+            this.MoveBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoveBar_MouseDown);
+            // 
+            // Minimize
+            // 
+            this.Minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Minimize.BackColor = System.Drawing.Color.DimGray;
+            this.Minimize.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Minimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minimize.Location = new System.Drawing.Point(441, 4);
+            this.Minimize.Margin = new System.Windows.Forms.Padding(0);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Padding = new System.Windows.Forms.Padding(1);
+            this.Minimize.Size = new System.Drawing.Size(23, 19);
+            this.Minimize.TabIndex = 5;
+            this.Minimize.Text = "-";
+            this.Minimize.UseVisualStyleBackColor = false;
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            // 
+            // AdditionBox
+            // 
+            this.AdditionBox.Location = new System.Drawing.Point(243, 180);
+            this.AdditionBox.MaxLength = 256;
+            this.AdditionBox.Name = "AdditionBox";
+            this.AdditionBox.Size = new System.Drawing.Size(141, 20);
+            this.AdditionBox.TabIndex = 6;
+            this.AdditionBox.WordWrap = false;
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AddButton.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.AddButton.Location = new System.Drawing.Point(391, 170);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(52, 39);
+            this.AddButton.TabIndex = 7;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = false;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(12, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 28);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Auto Windows Key Disabler";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoveBar_MouseDown);
+            // 
+            // RunAtStartupChkBox
+            // 
+            this.RunAtStartupChkBox.AutoSize = true;
+            this.RunAtStartupChkBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RunAtStartupChkBox.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.RunAtStartupChkBox.Location = new System.Drawing.Point(25, 58);
+            this.RunAtStartupChkBox.Name = "RunAtStartupChkBox";
+            this.RunAtStartupChkBox.Size = new System.Drawing.Size(93, 17);
+            this.RunAtStartupChkBox.TabIndex = 10;
+            this.RunAtStartupChkBox.Text = "Run at startup";
+            this.RunAtStartupChkBox.UseVisualStyleBackColor = false;
+            this.RunAtStartupChkBox.CheckedChanged += new System.EventHandler(this.RunAtStartupChkBox_CheckedChanged);
+            // 
+            // alphaBlendTextBox1
+            // 
+            this.alphaBlendTextBox1.BackAlpha = 10;
+            this.alphaBlendTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.alphaBlendTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.alphaBlendTextBox1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alphaBlendTextBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.alphaBlendTextBox1.Location = new System.Drawing.Point(243, 58);
+            this.alphaBlendTextBox1.Multiline = true;
+            this.alphaBlendTextBox1.Name = "alphaBlendTextBox1";
+            this.alphaBlendTextBox1.Size = new System.Drawing.Size(200, 76);
+            this.alphaBlendTextBox1.TabIndex = 12;
+            this.alphaBlendTextBox1.Text = "This will stop the windows key from executing while any application from the list" +
+    " is in the foreground.";
+            // 
+            // ApplicationListBox
+            // 
+            this.ApplicationListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ApplicationListBox.CausesValidation = false;
+            this.ApplicationListBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ApplicationListBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ApplicationListBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.ApplicationListBox.FormattingEnabled = true;
+            this.ApplicationListBox.ItemHeight = 20;
+            this.ApplicationListBox.Items.AddRange(new object[] {
+            "World of Warcraft",
+            "Valorant",
+            "League of Legends",
+            "Diablo",
+            "LostArk",
+            "Fortnite",
+            "Counter-Strike",
+            "Path of Exile",
+            "test",
+            "testtt",
+            "testtt33434"});
+            this.ApplicationListBox.Location = new System.Drawing.Point(25, 81);
+            this.ApplicationListBox.Name = "ApplicationListBox";
+            this.ApplicationListBox.Size = new System.Drawing.Size(188, 184);
+            this.ApplicationListBox.TabIndex = 13;
+            // 
+            // AutoWinWindow
+            // 
+            this.AcceptButton = this.AddButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackgroundImage = global::AutoKey.Properties.Resources.backgroundtest11;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(465, 276);
+            this.Controls.Add(this.ApplicationListBox);
+            this.Controls.Add(this.alphaBlendTextBox1);
+            this.Controls.Add(this.RunAtStartupChkBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.AdditionBox);
+            this.Controls.Add(this.Minimize);
+            this.Controls.Add(this.MoveBar);
+            this.Controls.Add(this.RemoveSelectedBtn);
+            this.Controls.Add(this.IsEnabledCheckbox);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutoWinWindow";
+            this.Text = "Automatic Windows Key Disable";
+            this.TransparencyKey = System.Drawing.Color.IndianRed;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox IsEnabledCheckbox;
+        private System.Windows.Forms.Button RemoveSelectedBtn;
+        private Button MoveBar;
+        private Button Minimize;
+        private TextBox AdditionBox;
+        private Button AddButton;
+        private Label label1;
+        private CheckBox RunAtStartupChkBox;
+        private ZBobb.AlphaBlendTextBox alphaBlendTextBox1;
+        private ListBox ApplicationListBox;
+    }
+}
+
---
+++ AutoWinForm.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AutoKey
+{
+    public partial class AutoWinWindow : Form
+    {
+        // Imports
+        // ---------------------------------
+        #region NativeImports
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern int GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(int hWnd, StringBuilder text, int count);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        #endregion
+
+        // Related to the imports
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        const int WH_KEYBOARD_LL = 13;
+        const int WM_KEYDOWN = 0x100;
+        private readonly LowLevelKeyboardProc llKeyProcess = ProcessEvent;
+        private static IntPtr hhook = IntPtr.Zero;
+
+
+        // Internals
+        // -------------------------------------
+
+        public static bool IsEnabled { get; private set; } = true;
+        public static List<string> autoWindow { get; private set; } = new List<string>();
+
+        private NotifyIcon trayIcon;
+
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public AutoWinWindow()
+        {
+            InitializeComponent();
+
+            // Configure Keyboard hook
+            hhook = SetWindowsHookEx(WH_KEYBOARD_LL, llKeyProcess, LoadLibrary("User32"), 0);
+
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = this.Icon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Maximize", RestoreWindow_Click)
+                ,new MenuItem("Exit", Exit)
+            }),
+                Visible = true
+            };
+
+            // Set the Event Listeners
+            this.FormClosing += AutoWinWindow_FormClosing;
+            this.Load += AutoWinWindow_Load;
+            ApplicationListBox.TextChanged += ApplicationListBox_TextChanged;
+
+            // Update the instance settings
+            configureVisualSettings();
+            updateProtectionList();
+
+        }
+
+        // -----------------------------
+
+        /// <summary>
+        /// Watches and intercepts the windows messages
+        /// </summary>
+        public static IntPtr ProcessEvent(int code, IntPtr wParam, IntPtr lParam)
+        {
+            // Only watch for KeyDown event messages
+            if (wParam == (IntPtr)WM_KEYDOWN)
+            {
+                // Get the virtual key code
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                // Check if the keypress was Windows Key
+                if (IsEnabled && vkCode == (int)Keys.LWin && IsProtectedWindow())
+                    return (IntPtr)1;
+
+                // Not the key we're watching ford
+                return (IntPtr)0;
+            }
+
+            return CallNextHookEx(hhook, code, (int)wParam, lParam);
+        }
+
+        // -----------------------------
+
+        /// <summary>
+        /// Customize some of the visual aspects
+        /// </summary>
+        void configureVisualSettings()
+        {
+            // Top bar
+            MoveBar.FlatStyle = FlatStyle.Flat;
+            MoveBar.FlatAppearance.BorderColor = BackColor;
+            MoveBar.FlatAppearance.MouseOverBackColor = BackColor;
+            MoveBar.FlatAppearance.MouseDownBackColor = BackColor;
+
+            // Set form values
+            this.RunAtStartupChkBox.Checked = Properties.Settings.Default.RunAtStartup;
+            this.IsEnabledCheckbox.Checked = Properties.Settings.Default.IsEnabled;
+            IsEnabled = this.IsEnabledCheckbox.Checked;
+        }
+
+        // ----------------------------
+
+        /// <summary>
+        /// Check if the foreground window is in the protected list
+        /// </summary>
+        internal static bool IsProtectedWindow()
+        {
+            // Get the list of active windows we're protecting
+            foreach (var item in autoWindow)
+            {
+                // If the title matches, consider it protected
+                if (GetActiveWindow().ToLowerInvariant().Contains(item.ToLowerInvariant()))
+                    return true;
+            }
+
+            return false;
+        }
+
+        // ----------------------------
+
+        /// <summary>
+        /// Gets the foreground (Active) Window
+        /// </summary>
+        private static string GetActiveWindow()
+        {
+            // Create the container
+            const int nChars = 256;
+            StringBuilder sb = new StringBuilder(nChars);
+
+            // Get the foreground window
+            int handle = GetForegroundWindow();
+            if (GetWindowText(handle, sb, nChars) > 0)
+                return sb.ToString();
+
+            // In case nothing was returned for some reason
+            return string.Empty;
+        }
+
+        #region FormEvents
+
+        // Whether this application is enabled
+        private void EnabledCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            IsEnabled = IsEnabledCheckbox.Checked;
+            Properties.Settings.Default.IsEnabled = IsEnabled;
+        }
+
+        // ---------------------------------------
+
+        // Custom window drag
+        private void MoveBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        // ---------------------------------------
+
+        // Minimize to sytem tray
+        void Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // ---------------------------------------
+
+        // Restore from system tray
+        void RestoreWindow_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        // ---------------------------------------
+
+        // Exit application
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+
+        // ---------------------------------------
+
+        private void RunAtStartupChkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // Set the registry Key
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            // Flip it
+            if (this.RunAtStartupChkBox.Checked)
+                regKey.SetValue("AutoWin", Application.ExecutablePath);
+            else
+                regKey.DeleteValue("AutoWin", false);
+
+            Properties.Settings.Default.RunAtStartup = this.RunAtStartupChkBox.Checked;
+        }
+
+        // ---------------------------------------
+
+        /// <summary>
+        /// Onload event when the form is created
+        /// </summary>
+        private void AutoWinWindow_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.StoredApplicationsList))
+            {
+                this.ApplicationListBox.Items.Clear();
+
+                Properties.Settings.Default.StoredApplicationsList.Split(',')
+                    .ToList()
+                    .ForEach(item =>
+                    {
+                        this.ApplicationListBox.Items.Add(item);
+                    });
+            }
+
+            Properties.Settings.Default.Reload();
+            updateProtectionList();
+        }
+
+        // ---------------------------------------
+
+        /// <summary>
+        /// Called when the form is exiting
+        /// </summary>
+        private void AutoWinWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var applications = this.ApplicationListBox.Items.Cast<string>().ToArray();
+            Properties.Settings.Default.StoredApplicationsList = string.Join(",", applications);
+            Properties.Settings.Default.Save();
+            trayIcon.Visible = false;
+        }
+
+        // ---------------------------------------
+
+        /// <summary>
+        /// Remove the selected index from the list
+        /// </summary>
+        private void RemoveSelectedBtn_Click(object sender, EventArgs e)
+        {
+            if (this.ApplicationListBox.SelectedItem != null)
+                this.ApplicationListBox.Items.RemoveAt(this.ApplicationListBox.SelectedIndex);
+
+            // Make sure we're using the addition
+            updateProtectionList();
+        }
+
+        // ---------------------------------------
+
+        /// <summary>
+        /// Add new Applications OnClick
+        /// </summary>
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            // If its blank, bail out
+            if (string.IsNullOrEmpty(AdditionBox.Text))
+                return;
+
+            // Avoid duplicate entries
+            bool hasEntry = false;
+            for (int i = 0; i < ApplicationListBox.Items.Count; i++)
+            {
+                if (ApplicationListBox.Items.Contains(AdditionBox.Text))
+                {
+                    hasEntry = true;
+                    break;
+                }
+            }
+
+            // Entry didn't exist, lets add it
+            if (!hasEntry)
+                this.ApplicationListBox.Items.Add(AdditionBox.Text);
+
+            // Make sure we're using the addition
+            updateProtectionList();
+
+            // Clear out the text box
+            AdditionBox.Text = String.Empty;
+        }
+
+        #endregion
+
+        // Random Stuff mostly
+        #region MiscHelpers
+
+        private void ApplicationListBox_TextChanged(object sender, EventArgs e)
+        {
+            updateProtectionList();
+        }
+
+        // ---------------------------------------
+
+        private void updateProtectionList()
+        {
+            // Move our values from the UI to the static list so native functions can access it
+            autoWindow.Clear();
+            for (int i = 0; i < this.ApplicationListBox.Items.Count; i++)
+            {
+                autoWindow.Add((string)this.ApplicationListBox.Items[i]);
+            }
+        }
+
+        #endregion
+
+    }
+}
